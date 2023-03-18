94ec1da0c2ec6c7a4a7c26288e3b1328e7787d89
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-03-18 18:40:35 -0300

    Buscar aluno por CPF

--- controllers/controller.go
+++ controllers/controller.go
@@ -68,3 +68,17 @@ func EditaAluno(c *gin.Context) {
 	database.DB.Model(&aluno).UpdateColumns(aluno)
 	c.JSON(http.StatusOK, aluno)
 }
+
+func BuscaAlunoPorCPF(c *gin.Context) {
+	var aluno models.Aluno
+	cpf := c.Param("cpf")
+	database.DB.Where(&models.Aluno{CPF: cpf}).First(&aluno)
+
+	if aluno.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"Not found": "Aluno não encontrado"})
+		return
+	}
+
+	c.JSON(http.StatusOK, aluno)
+}
--- routes/routes.go
+++ routes/routes.go
@@ -13,5 +13,6 @@ func HandleRequests() {
 	r.GET("/alunos/:id", controllers.BuscaAlunoPorID)
 	r.DELETE("/alunos/:id", controllers.DeletaAluno)
 	r.PATCH("/alunos/:id", controllers.EditaAluno)
+	r.GET("/alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
 	r.Run()
 }

47b06a839c27169fd0400128600153161197ea39
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-03-18 16:45:13 -0300

    Criar e usar Aluno Struct

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/brunosantanati/api-go-gin/routes"
+import (
+	"github.com/brunosantanati/api-go-gin/models"
+	"github.com/brunosantanati/api-go-gin/routes"
+)
 
 /*
 Rodar: go run main.go
@@ -8,5 +11,10 @@ Acessar: http://localhost:8080/
 */
 
 func main() {
+	models.Alunos = []models.Aluno{
+		{Nome: "Bruno Sant' Ana", CPF: "00000000000", RG: "320000000"},
+		{Nome: "Ana", CPF: "11111111111", RG: "330000000"},
+	}
+
 	routes.HandleRequests()
 }

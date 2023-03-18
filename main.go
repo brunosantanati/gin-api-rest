085fca94aedf7230098c13fab21527110c0da779
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-03-18 18:04:46 -0300

    Exibir todos alunos do DB

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/brunosantanati/api-go-gin/database"
-	"github.com/brunosantanati/api-go-gin/models"
 	"github.com/brunosantanati/api-go-gin/routes"
 )
 
@@ -13,10 +12,5 @@ Acessar: http://localhost:8080/
 
 func main() {
 	database.ConectaComBancoDeDados()
-	models.Alunos = []models.Aluno{
-		{Nome: "Bruno Sant' Ana", CPF: "00000000000", RG: "320000000"},
-		{Nome: "Ana", CPF: "11111111111", RG: "330000000"},
-	}
-
 	routes.HandleRequests()
 }

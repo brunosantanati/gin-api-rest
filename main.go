4b9e5e946c9cbb8d246a114887d3a5b872248695
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-03-18 16:02:07 -0300

    Criar primeira rota (endpoint)

--- main.go
+++ main.go
@@ -7,7 +7,15 @@ Rodar: go run main.go
 Acessar: http://localhost:8080/
 */
 
+func ExibeTodosAlunos(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"id":   "1",
+		"nome": "Bruno Sant' Ana",
+	})
+}
+
 func main() {
 	r := gin.Default()
+	r.GET("/alunos", ExibeTodosAlunos)
 	r.Run()
 }

bab88ca16e72b6463a8a5a8f1f4fc7419a9ddcb3
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-03-18 16:14:47 -0300

    Modularizar o codigo

--- main.go
+++ main.go
@@ -1,21 +1,12 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import "github.com/brunosantanati/api-go-gin/routes"
 
 /*
 Rodar: go run main.go
 Acessar: http://localhost:8080/
 */
 
-func ExibeTodosAlunos(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"id":   "1",
-		"nome": "Bruno Sant' Ana",
-	})
-}
-
 func main() {
-	r := gin.Default()
-	r.GET("/alunos", ExibeTodosAlunos)
-	r.Run()
+	routes.HandleRequests()
 }
---
+++ routes/routes.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	"github.com/brunosantanati/api-go-gin/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func HandleRequests() {
+	r := gin.Default()
+	r.GET("/alunos", controllers.ExibeTodosAlunos)
+	r.Run()
+}
